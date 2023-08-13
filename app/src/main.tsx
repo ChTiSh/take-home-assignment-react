a01ec222de286438494f62fc9dd51c5647228b1d
ChTiSh <chentina.shen@gmail.com>
2023-08-13 10:07:02 -0400

    initial setup for errorlink

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, from} from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import Login from './components/Login.tsx'
 import App from './App.tsx'
 import { AUTH_TOKEN } from "./constants.ts";
 import ProductList from './components/ProductList.tsx'
 import './index.css'
+import { onError } from "@apollo/client/link/error";
+
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:8080/graphql',
@@ -28,9 +30,39 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const errorLink = onError(
+  ({ graphQLErrors, networkError, operation, forward }) => {
+    if (graphQLErrors) {
+      for (const err of graphQLErrors) {
+        switch (err.extensions.code) {
+          // Apollo Server sets code to UNAUTHENTICATED
+          // when an AuthenticationError is thrown in a resolver
+          case "UNAUTHENTICATED":{
+            // Modify the operation context with a new token
+            const oldHeaders = operation.getContext().headers;
+            operation.setContext({
+              headers: {
+                ...oldHeaders,
+                authorization: getNewToken(),
+              },
+            });
+            // Retry the request, returning the new observable
+            return forward(operation);
+          }
+        }
+      }
+    }
+  
+    // To retry on network errors, we recommend the RetryLink
+    // instead of the onError link. This just logs the error.
+    if (networkError) {
+      console.log(`[Network error]: ${networkError}`);
+    }
+  }
+);
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  link: from([httpLink, authLink, errorLink]),
 });
 
 const router = createBrowserRouter([
